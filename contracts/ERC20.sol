b48a442e0ae565cde868de7eaa16cf71557ace4f
ljankovic-txfusion <lazar@txfusion.io>
2023-05-04 10:51:46 +0200

    remove ownable from ERC20

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyERC20 is ERC20, Ownable {
+contract MyERC20 is ERC20 {
     constructor() ERC20("MyToken", "MTK") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
